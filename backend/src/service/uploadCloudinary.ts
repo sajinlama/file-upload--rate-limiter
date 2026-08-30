d6b75fda316384fa9fa5530d3c0902be19cdfc29
sajinlama <tamangsajin99@gmail.com>
2026-08-30 20:29:41 +0545

    deployment ready

--- backend/src/service/uploadCloudinary.ts
+++ backend/src/service/uploadCloudinary.ts
@@ -15,14 +15,18 @@ cloudinary.config({
   api_secret: CLOUDINARY_API_SECRET,
 }); 
 
-export const uploadToCloudinary = async (filePath: string, publicId?: string) => {
+export const uploadToCloudinary = async (
+  filePath: string,
+  publicId?: string
+) => {
   try {
     const uploadResult = await cloudinary.uploader.upload(filePath, {
-      public_id: publicId,
+      ...(publicId && { public_id: publicId }),
     });
+
     return uploadResult;
   } catch (error) {
-    console.error('Cloudinary upload failed:', error);
+    console.error("Cloudinary upload failed:", error);
     throw error;
   }
 };
